90b81698a6a412d1b27ff9978d95e5250012547d
dovanthao2404 <dovanthao2404@gmail.com>
2021-11-28 11:25:28 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,6 +10,7 @@ export const environment = {
   token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOT0RFSlMgMTgiLCJIZXRIYW5TdHJpbmciOiIwMy8wNS8yMDIyIiwiSGV0SGFuVGltZSI6IjE2NTE1MzYwMDAwMDAiLCJuYmYiOjE2MzUwOTQ4MDAsImV4cCI6MTY1MTY4MzYwMH0.WR33CYjpZsWp64GNg_ygfcOGHHNsT5DomeXdzWKxRyg",
   tokenCybersoft: "TokenCybersoft",
   userLogin: "USER_LOGIN",
+  listCourseRegister: "LIST_COURSE",
   authorization: "Authorization",
   accessToken: "accessToken",
   GP_ID,
